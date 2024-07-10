76607c3620dec5e97f6b3fb54f977a43159c116f
JotaMacLane <jpnascimentoa@gmail.com>
2024-07-09 22:06:53 -0300

    Iniciando JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-const horas = document.querySelector(".horas");
-const minutos = document.querySelector(".minutos");
-const segundos = document.querySelector(".segundos");
-let hora=0, minuto=0, segundo=0;
-function init(){
-    if(segundos== 60){
-        minutos= minutos++;
-        segundos = 0
-    }
-    if(minutos==60){
-        horas= horas++;
-        minutos =0
-    }
+const playBtn = document.querySelector('#play');
+const reset = document.getElementsByClassName('reset');
+
+
+const windowsHora = document.querySelector(".horas");
+const windowsMinuto = document.querySelector(".minutos");
+const windowsSegundo = document.querySelector(".segundos");
+const windowsMiliSegundo = document.querySelector(".milisegundos");
+
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
+let milisegundos = 0;
+let interval = 0;
+let pause = true
+// funcões
+
+playBtn.addEventListener('click', initTimer);
+
+
+function initTimer(){
+    let togglePlayPause = playBtn.getAttribute('action')
+    interval = setInterval(()=>{
+        milisegundos += 10;
+
+        if(togglePlayPause == "start"|| togglePlayPause == "continue"){
+            if(milisegundos === 1000){
+                segundos++;
+                milisegundos=0;
+            }
+            if(segundos===60){
+                minutos++;
+                segundos= 0;
+            }
+            if(minutos===60){
+                horas++;
+                minutos =0;
+            }
+            windowsHora.textContent= horas;
+            windowsMinuto.textContent= minutos;
+            windowsSegundo.textContent= segundos;
+            windowsMiliSegundo.textContent = milisegundos;
+            
+            playBtn.addEventListener('click', pausetimer);
+            function pausetimer(){
+                togglePlayPause = playBtn.setAttribute("action", "pause")
+                
+            }
+            
+        }
+        },10)
     
 }
+
+
